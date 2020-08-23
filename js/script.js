56cc4a59a48808c25b4becbf987029120e5105f5
Silas Rodrigues <47532703+SilasRodrigues19@users.noreply.github.com>
2020-08-23 10:24:49 -0300

    Update JS
    Adicionada função tooltip do bootstrap e alterada data-word

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Web Developer", "Web Designer", "Free Lancer"];
+const textArray = ["Web Developer", "Programador", "Free Lancer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
@@ -39,3 +39,10 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
+
+/* Alert */
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();   
+});
+

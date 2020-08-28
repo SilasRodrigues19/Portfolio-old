e187eeaecdce4c61508c25de2f4226dc27ed23ad
Silas Rodrigues <47532703+SilasRodrigues19@users.noreply.github.com>
2020-08-28 15:22:05 -0300

    Update script.js
    Função para exibit o navbar só apos scrollar a tela

--- js/script.js
+++ js/script.js
@@ -71,3 +71,30 @@ document.addEventListener("DOMContentLoaded", function() {
     loader = document.getElementById('loader');
     loadNow(1);
 });
+
+const menu = document.querySelector('.header .nav-bar .nav-list .menu');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = '#434343';
+    
+  } else {
+    header.style.backgroundColor = 'transparent';
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+  });
+});

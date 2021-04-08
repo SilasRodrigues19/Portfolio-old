e6360b71f6feb30fcf90143abead0e161c729946
Silas Rodrigues <47532703+SilasRodrigues19@users.noreply.github.com>
2021-04-07 23:43:56 -0300

    Rename ID

--- js/smoth-scroll.js
+++ js/smoth-scroll.js
@@ -1,6 +1,6 @@
 /* Smoth scroll */
  jQuery(document).ready(function($) {
-   $("#back").click(function(event){
+   $("#smothScroll").click(function(event){
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 3500);
    });

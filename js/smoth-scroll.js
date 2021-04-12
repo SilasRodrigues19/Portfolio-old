05e17cc654df85ca28fddbd8b5d7d4fd2d2fd70d
Silas Rodrigues <47532703+SilasRodrigues19@users.noreply.github.com>
2021-04-11 22:09:23 -0300

    Menu items update
    Animação smooth adicionada aos itens do menu

--- js/smoth-scroll.js
+++ js/smoth-scroll.js
@@ -2,6 +2,11 @@
  jQuery(document).ready(function($) {
    $("#smothScroll").click(function(event){
     event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 3500);
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 2500);
+   });
+
+   $(".scroll").click(function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 3000);
    });
   });

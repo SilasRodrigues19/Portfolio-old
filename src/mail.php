5ea8e5eb630035302cbf359780ada8472c63f34b
Silas Rodrigues <47532703+SilasRodrigues19@users.noreply.github.com>
2021-05-01 06:56:44 -0300

    Update

--- src/mail.php
+++ src/mail.php
@@ -16,6 +16,7 @@
 <body>
 
 <?php 
+session_start();
 
 require_once('PHPMailer.php');
 require_once('SMTP.php');
@@ -31,68 +32,76 @@ $email = $_POST['email'];
 $celular = $_POST['celular'];
 $mensagem = $_POST['mensagem'];
 
-$mail = new PHPMailer(true);
+$resultBtn = filter_input(INPUT_POST, 'btnSubmit', FILTER_SANITIZE_STRING);
 
-try {
-	// Server Settings
-	//$mail->SMTPDebug = SMTP::DEBUG_SERVER; //verbose debug output
-	$mail->isSMTP(); // To use SMTP
-	$mail->Host = 'smtp.gmail.com'; // Change if u don't use Gmail
-	$mail->SMTPAuth = true; // Authentication
-	$mail->Username = 'example@gmail.com'; // Your domain e-mail
-	$mail->Password = 'yourpassword'; // Your pass
-	$mail->Port = 587; // Gmail default port
+// Se o usuário clicou no botão para enviar o formulário
+if ($resultBtn) {
+	$mail = new PHPMailer(true);
 
-	// Recipients
-	$mail->setFrom('silasrodrigues.fatec@gmail.com'); // Name optional example ('example@gmail.com', 'Your Name');
-	$mail->addAddress('silasrodrigues.fatec@gmail.com'); // Name optional example ('example@gmail.com', 'Your Name');
+	try {
+		// Server Settings
+		//$mail->SMTPDebug = SMTP::DEBUG_SERVER; //verbose debug output
+		$mail->isSMTP(); // To use SMTP
+		$mail->Host = 'smtp.gmail.com'; // Change if u don't use Gmail
+		$mail->SMTPAuth = true; // Authentication
+		$mail->Username = 'silasrodrigues.fatec@gmail.com'; // Your domain e-mail
+		$mail->Password = 'gawb23132new'; // Your pass
+		$mail->Port = 587; // Gmail default port
 
-
-	// Content
-	$mail->isHTML(true); // Set HTML format
-	$mail->Subject = 'Contato - Portfolio';
-	$mail->Body = "<div style='padding: 2rem 1rem; margin: 1rem 4rem 1rem 1rem; background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898;
- background-blend-mode: multiply,multiply; border-radius: 0.3rem;'><p style='color:#fff;'><strong>Nome: </strong>$nome $sobrenome</p> <hr><br><p style='color:#fff'><strong>Email: </strong>$email</p> <hr><br><p style='color:#fff;'><strong>Celular: </strong>$celular</p> <hr><br><p style='color:#fff;'><strong>Mensagem: </strong>$mensagem</p></div>";
+		// Recipients
+		$mail->setFrom('silasrodrigues.fatec@gmail.com'); // Name optional example ('example@gmail.com', 'Your Name');
+		$mail->addAddress('silasrodrigues.fatec@gmail.com'); // Name optional example ('example@gmail.com', 'Your Name');
 
 
-?>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
-<?php 
+		// Content
+		$mail->isHTML(true); // Set HTML format
+		$mail->Subject = 'Contato - Portfolio';
+		$mail->Body = "<div style='padding: 2rem 1rem; margin: 1rem 4rem 1rem 1rem; background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898;
+	 background-blend-mode: multiply,multiply; border-radius: 0.3rem;'><p style='color:#fff;'><strong>Nome: </strong>$nome $sobrenome</p> <hr><br><p style='color:#fff'><strong>Email: </strong>$email</p> <hr><br><p style='color:#fff;'><strong>Celular: </strong>$celular</p> <hr><br><p style='color:#fff;'><strong>Mensagem: </strong>$mensagem</p></div>";
 
-	if ($mail->send()) {
+
+	?>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+	<?php 
+
+		if ($mail->send()) {
+			?>
+			<script>
+				swal({
+				  title: "Bom trabalho!",
+				  text: "Seu email foi enviado com sucesso",
+				  icon: "success",
+				});
+				setTimeout(function(){location.href="../public/index.php"} , 5000); 
+			</script>
+			<?php
+		} else {
+			?>
+			<script>
+				sweetAlert("Ooops...", "Seu email não foi enviado", "error");
+				setTimeout(function(){location.href="../public/index.php"} , 5000);
+			</script>
+			<?php
+		}
+
+	} catch (Exception $e) {
+		echo "Erro ao enviar a mensagem: {$mail->ErrorInfo}";
 		?>
 		<script>
 			swal({
-			  title: "Bom trabalho!",
-			  text: "Seu email foi enviado com sucesso",
-			  icon: "success",
+			  title: "Erro de codificação!",
+			  text: "Contate um administrador!",
+			  icon: "error",
 			});
-			setTimeout(function(){location.href="../public/index.html"} , 5000); 
-		</script>
-		<?php
-	} else {
-		?>
-		<script>
-			sweetAlert("Ooops...", "Seu email não foi enviado", "error");
-			setTimeout(function(){location.href="../public/index.html"} , 5000);
+			setTimeout(function(){location.href="../public/index.php"} , 5000);
 		</script>
 		<?php
 	}
-
-} catch (Exception $e) {
-	echo "Erro ao enviar a mensagem: {$mail->ErrorInfo}";
-	?>
-	<script>
-		swal({
-		  title: "Erro de codificação!",
-		  text: "Contate um administrador!",
-		  icon: "error",
-		});
-		setTimeout(function(){location.href="../public/index.html"} , 5000);
-	</script>
-	<?php
+  // Caso contrário, ou seja, não clicou no botão e acessou a URL vai exibit uma caixa de alerta informando que não da pra acessar via URL direto	
+} else {
+	$_SESSION['msg'] = "<p id='psection' style='position:fixed; top: 90px; z-index:9999; color:#000; text-transform:uppercase; border-radius:10px; animation:fadeInDown 4s;' class='alert alert-warning w-50 text-center'>Acesso negado<br> Preencha o formulário e clique em enviar ao invés de acessar via URL <br> <button id='btnsection' class='btn btn-outline-danger w-100'>Fechar</button></p>";
+	header("Location: ../public/index.php");
 }
-
 
 ?>
 

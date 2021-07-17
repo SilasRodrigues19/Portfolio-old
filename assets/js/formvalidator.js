6fd66fa147ea73b4a6105ae345d026695111751e
Silas Rodrigues <47532703+SilasRodrigues19@users.noreply.github.com>
2021-07-17 16:48:34 -0300

    Create formvalidator.js
    - Form validator deixa de ser uma função inline, agora sendo exportada

---
+++ assets/js/formvalidator.js
@@ -0,0 +1,53 @@
+jQuery(function ($) {
+    $.validator.addMethod("alphabetsnspace", function(value, element) {
+          return this.optional(element) || /^[a-zA-ZÀ-ú' ]*$/.test(value);
+      });
+
+  $("#formContact").validate({
+    rules: {
+       nome: {
+        alphabetsnspace: true,
+        minlength: 2,
+        maxlength: 15,
+        required: true
+
+       },
+       sobrenome: {
+        required: true,
+        minlength: 3,
+        maxlength: 20,
+        alphabetsnspace: true
+       },
+       email: {
+        required: true,
+        email: true
+       },
+       celular: {
+        required: true,
+        minlength: 15
+       },
+       mensagem: {
+        required: true,
+        maxlength: 450
+       }
+    },
+    messages:{
+       nome: {
+        required:"Por favor, informe seu nome",
+        alphabetsnspace: "Por favor, apenas letras"
+       },
+       sobrenome: {
+        required:"Por favor, informe seu sobrenome",
+        alphabetsnspace: "Por favor, apenas letras"
+       },
+       email: {
+        required:"Por favor, informe seu e-mail para contato"
+       },
+       celular: {
+        required:"Por favor, informe seu número de celular",
+        minlength:"Por favor, insira 11 dígitos (somente números)"
+       }
+      }
+  })
+});
+$("#celular").mask("(00) 00000-0000");

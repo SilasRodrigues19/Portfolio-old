825353070963dfe7ccd19f3c138dc06ae72241be
Silas Rodrigues <47532703+SilasRodrigues19@users.noreply.github.com>
2021-08-02 02:10:21 -0300

    🖌 Styles Update
    - New color for header and footer;
    - New back to top button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,9 +82,8 @@ const menu = document.querySelector('.header .nav-bar .nav-list .menu');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
-const hamburger = document.querySelector('.scroll').addEventListener('click', () => {
 
-});
+let scrollToTop = document.querySelector('#smothScroll');
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('active');
@@ -95,10 +94,16 @@ menu.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
   var scroll_position = window.scrollY;
   if (scroll_position > 100) {
-    header.style.backgroundColor = '#0f0e17';
+    $(header).css('backgroundColor', '#111');
 
   } else {
-    header.style.backgroundColor = 'transparent';
+    $(header).css('backgroundColor', 'transparent');
+  }
+
+  if(scroll_position < 600) {
+    $(scrollToTop).css('opacity', '0');
+  } else {
+    $(scrollToTop).css('opacity', '1');
   }
 });
 
@@ -106,7 +111,7 @@ menu_item.forEach((item) => {
   item.addEventListener('click', () => {
     menu.classList.toggle('active');
     mobile_menu.classList.toggle('active');
-    // Fix Menu Error 
+    // Fix Menu Error
     if (!mobile_menu.classList.contains('active')) {
       $('#hamburger').prop("checked", false);
     } else {

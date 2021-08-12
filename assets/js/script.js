e7c81eb292e79c14f04f1ccb1bc8270dba3c1909
Silas Rodrigues <47532703+SilasRodrigues19@users.noreply.github.com>
2021-08-12 16:18:10 -0300

    Testing commit from VSCODE on browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,9 +81,6 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-
-
-
 const scrollToTop = document.querySelector('#smoothScroll');
 const social = document.querySelector('.social ul');
 
